4a310dce6e2d62e68b1a0913dbac7fd71fa7561d
AdrianaHY <adriana.herya@gmail.com>
2017-06-12 11:10:09 -0500

    plantilla

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 var cargarPagina = function(){
   cargarPersonajes();
+  //cuando los elementos son creados dinamicamente hay que utilizar evento on
+  $(document).on("click",".personaje",mostrarDetallePersonaje);
 };
 
 // todo esto es para no realizar el servidor
@@ -23,8 +25,24 @@ var mostrarPersonajes = function(personajes){
   var $ul = $("#personajes");
   personajes.forEach(function(personaje){
     var $li = $("<li />");
+    $li.addClass("personaje");
+    $li.attr("data-url",personaje.homeworld);
     $li.text(personaje.name + "-" + personaje.height + " cm");
     $ul.append($li);
+  });
+};
+
+var plantillaPlaneta = "<h2>Planeta:</h2>"+
+"<p><strong>Nombre:</strong>__nombre__</p>"+
+"<p><strong>Clima:</strong>__clima__</p>";
+
+var mostrarDetallePersonaje = function(){
+  var url = ($(this).data("url"));
+  var $planetaContenedor = $("#planeta");
+  $.getJSON(url, function(response){
+    $planetaContenedor.html(plantillaPlaneta.replace("__nombre__", response.name)
+      .replace("__clima__",response.climate));
+
   });
 };
 
